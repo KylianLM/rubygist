c4deee3aa33e12c178fc75f659a46427000b1229
Kylian LE METTE <kylian.le.mette@gmail.com>
2016-03-17 09:44:42 +0100

    #2 get gist from gist

--- app/controllers/gist_controller.rb
+++ app/controllers/gist_controller.rb
@@ -1,4 +1,5 @@
 class GistController < ApplicationController
+  require 'net/http'
   def home
   end
 
@@ -9,5 +10,11 @@ class GistController < ApplicationController
   def create
     Gist.create id_gist: params[:id_gist], url: "https://api.github.com/gists/" + params[:id_gist]
     redirect_to "/gist"
+  end
+
+  def test
+    uri = URI('https://api.github.com/gists/47601c9bd553750e0dce')
+    res = Net::HTTP.get(uri)
+    puts res
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'gist#home'
+  root 'gist#test'
   get 'gist' => 'gist#home'
   get 'gist/add' => 'gist#index'
   post 'gist/add' => 'gist#create'
