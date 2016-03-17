7e0adc042dbb0596fcb0dc2131798f726db1067b
Kylian LE METTE <kylian.le.mette@gmail.com>
2016-03-17 23:23:31 +0100

    #7 Feature Search Category

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'gist#home'
   get 'gist' => 'gist#home'
+  post 'gist' => 'gist#category'
   get 'gist/add' => 'gist#index'
   post 'gist/add' => 'gist#create'
 
